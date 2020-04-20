a3a465da867b83a27e323750e879e3afc784abb5
Peng Hao <peng.hao@elulagroup.com>
2020-04-20 17:17:28 +1000

    make a change to welcome message on root page

--- hello_world_web_application/main.py
+++ hello_world_web_application/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World, my first web application!!'
 
 @app.errorhandler(500)
 def server_error(e):
